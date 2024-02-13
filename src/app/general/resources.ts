7c36cf515e3fa6e8ae74545765b20dae6784cf72
Shlomi Shitrit <akunh113@gmail.com>
2024-02-13 11:07:24 +0200

    fix text alignment

--- src/app/general/resources.ts
+++ src/app/general/resources.ts
@@ -4,18 +4,19 @@ import { sender, typeOfQuestion } from "@/app/general/types";
 export const botMessages: Message[] = [
     {
         id: 0,
-        text: `Hello! I'm a ChatBot that will help you to get small pieces of data 
-from a large dataset called the English Lexicon Project. 
-The English Lexicon Project contains a large number of words 
-with many of their properties, and summaries of behavioral data 
-from experiments that examined how easy or difficult to read these words are. 
+        text: `Hello! I'm a ChatBot that will help you to get small pieces of data from a large dataset called the English Lexicon Project. 
+
+The English Lexicon Project contains a large number of words with many of their properties, and summaries of behavioral data from experiments that examined how easy or difficult to read these words are. 
+
 My main purpose as a ChatBot is to help you get data based on specific properties of words.
 For example, you may be interested in looking specifically at very long or very short words, 
 or to look at words that are acquired early or late in childhood.
- At the moment, I have the capacity of helping you extract data based on three word properties:
+
+At the moment, I have the capacity of helping you extract data based on three word properties:
 1: Age of Acquisition
 2: Number of Phonemes
 3: Number of Syllables.
+
 Which property would you like to start with?`,
         sender: "bot",
         typeOfQuestion: "parameter",
@@ -26,12 +27,15 @@ Which property would you like to start with?`,
         text: `To help you get the words you desire, 
 I need to know few things about your specific 
 requirments to the property you have chosen above.
+
 First I need to know if you want the words to be greater, lower or equal to a specific value.
 Then I need to know the value of this property you want to start with.
-For example, if you chose the property "age of aquisition" 
-and you want words that aquiered at the age of 5 or less,
-you will choose the "Lower" in this question, and then the value 5 in the next question.
-So, Do you want the words to be greater, lower or equal to a specific value?
+
+For example, if you chose the property "age of aquisition" and you want words that aquiered at the age of 5 or less, you will choose the "Lower" in this question, and then the value 5 in the next question.
+
+Let's start with the first question.
+Do you want the words to be greater, lower or equal to a specific value?
+
 1. Greater
 2. Lower
 3. Equal`,
@@ -42,6 +46,7 @@ So, Do you want the words to be greater, lower or equal to a specific value?
     {
         id: 2,
         text: `Now I need to know the value of this parameter you want to start with.
+        
 What would you like the value of this property to be?`,
         sender: "bot",
         typeOfQuestion: "value",
